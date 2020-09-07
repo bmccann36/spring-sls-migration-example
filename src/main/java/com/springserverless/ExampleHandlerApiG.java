7bb561a54711a76559140550f7dd54b38f9b64bd
chylomicronman <bmccann36@gmail.com>
2020-09-07 08:06:08 -0400

    added sample handlers

--- src/main/java/com/springserverless/ExampleHandlerApiG.java
+++ src/main/java/com/springserverless/ExampleHandlerApiG.java
@@ -6,20 +6,27 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.Collections;
 import java.util.Map;
 
-public class ExampleHandler implements RequestHandler<Map<String,String>, String> {
+public class ExampleHandlerApiG implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
     private Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
 
     @Override
-    public String handleRequest(Map<String,String> event, Context context) {
+    public ApiGatewayResponse handleRequest(Map<String, Object> event, Context context) {
         LambdaLogger logger = context.getLogger();
         String response = new String("200 OK");
         // process event
         logger.log("EVENT: " + event);
         logger.log("\n EVENT TYPE: " + event.getClass().toString() + "\n");
-        return response;
+
+        // send the response back
+        return ApiGatewayResponse.builder()
+                .setStatusCode(200)
+                .setObjectBody("function executed")
+                .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                .build();
     }
 }
---
+++ src/main/java/com/springserverless/ExampleHandlerStream.java
@@ -0,0 +1,52 @@
+package com.springserverless;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExampleHandlerStream implements RequestStreamHandler {
+
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        LambdaLogger logger = context.getLogger();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("US-ASCII")));
+        PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream, Charset.forName("US-ASCII"))));
+        Map event = new HashMap();
+        try {
+            event = gson.fromJson(reader, HashMap.class);
+            logger.log("STREAM TYPE: " + inputStream.getClass().toString());
+            logger.log("EVENT TYPE: " + event.getClass().toString());
+            logger.log("\nEVENT\n");
+            logger.log(gson.toJson(event) + "\n");
+            //? wrap response as api gateway response
+            ApiGatewayResponse apigRes = ApiGatewayResponse.builder()
+                    .setStatusCode(200)
+                    .setObjectBody(event)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+            //? write response to writer
+            writer.write(gson.toJson(apigRes));
+
+            if (writer.checkError()) {
+                logger.log("WARNING: Writer encountered an error.");
+            }
+        } catch (IllegalStateException | JsonSyntaxException exception) {
+            logger.log(exception.toString());
+        } finally {
+            reader.close();
+            writer.close();
+        }
+    }
+}
